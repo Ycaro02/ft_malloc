5a9f832dab6c409fafa5029fbc168e26b0ee96d3
Ycaro02 <94980706+Ycaro02@users.noreply.github.com>
2023-10-28 21:01:30 +0200

    Update main.c finish test realloc

--- src/main.c
+++ src/main.c
@@ -8,7 +8,8 @@ void realloc_test(int max)
 {
   char *test = NULL;
   for ( int i = 0; i < max; i++)
-
+    test = realloc(sizeof(char) * (100 * (i + 1));
+  show_alloc_mem();
 }
 
 void basic_test(int max, int len)
