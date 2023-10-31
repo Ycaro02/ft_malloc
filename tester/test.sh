20071fb58431600830ed143f08b4a5bf6e950e0e
Ycaro02 <nicolas@Nicolas-PC.localdomain>
2023-10-31 14:34:25 +0100

    clean test version

--- tester/test.sh
+++ tester/test.sh
@@ -3,13 +3,10 @@
 REAL="real/"
 ME="me/"
 
-# if [ -f ${REAL}${1}.c ] && [ -f ${ME}${2}.c ];
-# then
+if [ -f ${REAL}${1}.c ] && [ -f ${ME}${2}.c ];
+then
     make -s -C .. re > /dev/null
     mkdir -p .tmp
-    echo HEYEHEYEYEYEYEY
-    echo HEYEHEYEYEYEYEY
-    echo HEYEHEYEYEYEYEY
     gcc -o ${1}_real ${REAL}${1}.c
     gcc -o ${2}_me ${ME}${2}.c ../libft_malloc.so
     /usr/bin/time -v ./${1}_real 2> .tmp/out_real${1}
@@ -22,9 +19,9 @@ ME="me/"
     echo "page for my malloc :"
     cat .tmp/out_me${2} | grep Minor
     rm -rf .tmp ${1}_real ${2}_me
-# else
-    # echo File ${REAL}$1.c or ${ME}$2.c not found
-# fi
+else
+    echo File ${REAL}$1.c or ${ME}$2.c not found
+fi
 
 
 # echo at begin hosttype = $HOSTTYPE
